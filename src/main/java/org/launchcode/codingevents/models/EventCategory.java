05f69cc0b91fa6e123c669dbcc25d81d27bad6d9
Carl <carljarms@gmail.com>
2020-03-02 18:26:42 -0600

    Completed Class 16 Studio

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -7,26 +7,17 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Entity
-public class EventCategory {
-
-    @Id
-    @GeneratedValue
-    private int id;
+public class EventCategory extends AbstractEntity{
 
     @NotBlank(message = "Name is required")
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
 
-    public EventCategory(int id, String name) {
-        this.id = id;
+    public EventCategory(String name) {
         this.name = name;
     }
 
     public EventCategory() {}
-
-    public int getId() {
-        return id;
-    }
 
     public String getName() {
         return name;
